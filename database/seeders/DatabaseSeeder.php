55fff7982382daa93396ced72ccdf1abb4b08e77
aep <aepsaprul18@gmail.com>
2023-06-16 16:45:35 +0700

    installation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // $this->call(PermissionTableSeeder::class);
+        // $this->call(CreateAdminUserSeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
